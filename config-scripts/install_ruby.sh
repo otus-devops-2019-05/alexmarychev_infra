15e386e6e30a2e169a8ec5c0abf3358c0d3d86db
Alexander Marychev <asu-linux@mail.ru>
2019-06-30 16:51:05 +0300

    HW#5

--- config-scripts/install_ruby.sh
+++ config-scripts/install_ruby.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+set -e
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
---
+++ packer/scripts/install_ruby.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
